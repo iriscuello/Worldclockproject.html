65de32bbbcff9723aa2261f688fd47ab7d6bb7ff
Iris Linares <iriscuello@yahoo.com>
2022-11-04 19:41:50 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -2,4 +2,4 @@ let shangElement = document.querySelector("#city-shang");
 let shangDateElement = document.querySelector(".date");
 let shangTimeElement = document.querySelector(".time");
 shangDateElement.innerHTML = moment().format("MMMM Do YYYY");
-shangTimeElement.innerHTML = moment.().format("h:m:s A")
+shangTimeElement.innerHTML = moment().format("h:m:s A");
