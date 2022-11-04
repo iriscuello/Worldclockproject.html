ce0204615b18dc0a8c8c2401067d09edc7bcbc02
Iris Linares <iriscuello@yahoo.com>
2022-11-04 19:28:29 -0400

    java update

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,3 @@
 let shangElement = document.querySelector("#city-shang");
 let shangDateElement = document.querySelector(".date");
-shangElement.innerHTML = "Happy";
+shangDateElement.innerHTML = "Happy";
