d24c36c442dd5f98c89cb4c68e2c85a9d97f2610
Iris Linares <iriscuello@yahoo.com>
2022-11-04 19:53:09 -0400

    Update index.js
    added date and time element using moment

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,7 @@
 let shangElement = document.querySelector("#city-shang");
 let shangDateElement = document.querySelector(".date");
 let shangTimeElement = document.querySelector(".time");
-shangDateElement.innerHTML = moment().format("MMMM Do YYYY");
-shangTimeElement.innerHTML = moment().format("h:m:ss");
+let shangTime = moment();
+
+shangDateElement.innerHTML = shangTime.format("MMMM Do, YYYY");
+shangTimeElement.innerHTML = shangTime.format("h:m:ss [<small>]a[</small>]");
