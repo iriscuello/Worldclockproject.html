f19a43065a82f0a05dcc4ec88f96c83e7f6e79b7
Iris Linares <iriscuello@yahoo.com>
2022-11-04 21:06:19 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,15 @@
 function realTime() {
   let shangElement = document.querySelector("#city-shang");
-  let shangDateElement = document.querySelector(".date");
-  let shangTimeElement = document.querySelector(".time");
+  let shangDateElement = shangElement.querySelector(".date");
+  let shangTimeElement = shangElement.querySelector(".time");
   let shangTime = moment().tz("Asia/Shanghai");
 
   shangDateElement.innerHTML = shangTime.format("MMMM Do, YYYY");
   shangTimeElement.innerHTML = shangTime.format("h:mm:ss [<small>]a[</small>]");
 
   let seoulElement = document.querySelector("#citySeoul");
-  let seoulDateElement = document.querySelector(".date");
-  let seoulTimeElement = document.querySelector(".time");
+  let seoulDateElement = seoulElement.querySelector(".date");
+  let seoulTimeElement = seoulElement.querySelector(".time");
   let seoulTime = moment().tz("Asia/Seoul");
 
   seoulDateElement.innerHTML = seoulTime.format("MMMM Do, YYYY");
