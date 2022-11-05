072560d96463570dc706e1732043123eef0956c8
Iris Linares <iriscuello@yahoo.com>
2022-11-05 18:33:44 -0400

    updated html and JS
    added time zone function

--- javascript/index.js
+++ javascript/index.js
@@ -21,20 +21,19 @@ function updateCity(event) {
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
-  <div>
+        <div class="city">
           <div>
-            <h2 >${cityTimeZone}</h2>
+            <h2>${cityTimeZone}</h2>
             <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
           </div>
-          <div class="time">${cityTime.format(
-            "h:mm:ss"
-          )} <SMALL>${cityTime.format("A")}</SMALL></div>
-        </div>
-        `;
+          <div class="time">
+            ${cityTime.format("h:mm:ss")} <SMALL>${cityTime.format("A")}</SMALL>
+          </div>
+        </div>`;
 }
+
 realTime();
 setInterval(realTime, 1000);
 
 let citiesSelectElement = document.querySelector("#options");
-
 citiesSelectElement.addEventListener("change", updateCity);
