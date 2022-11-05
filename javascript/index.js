b3c3fe2ffc5cb406458c600d0e372ac9df6d4d6b
Iris Linares <iriscuello@yahoo.com>
2022-11-05 19:14:45 -0400

    Update index.js
    added if statment

--- javascript/index.js
+++ javascript/index.js
@@ -1,19 +1,27 @@
 function realTime() {
   let shangElement = document.querySelector("#city-shang");
-  let shangDateElement = shangElement.querySelector(".date");
-  let shangTimeElement = shangElement.querySelector(".time");
-  let shangTime = moment().tz("Asia/Shanghai");
+  if (shangElement) {
+    let shangDateElement = shangElement.querySelector(".date");
+    let shangTimeElement = shangElement.querySelector(".time");
+    let shangTime = moment().tz("Asia/Shanghai");
 
-  shangDateElement.innerHTML = shangTime.format("MMMM Do, YYYY");
-  shangTimeElement.innerHTML = shangTime.format("h:mm:ss [<small>]a[</small>]");
+    shangDateElement.innerHTML = shangTime.format("MMMM Do, YYYY");
+    shangTimeElement.innerHTML = shangTime.format(
+      "h:mm:ss [<small>]a[</small>]"
+    );
+  }
 
   let seoulElement = document.querySelector("#citySeoul");
-  let seoulDateElement = seoulElement.querySelector(".date");
-  let seoulTimeElement = seoulElement.querySelector(".time");
-  let seoulTime = moment().tz("Asia/Seoul");
+  if (seoulElement) {
+    let seoulDateElement = seoulElement.querySelector(".date");
+    let seoulTimeElement = seoulElement.querySelector(".time");
+    let seoulTime = moment().tz("Asia/Seoul");
 
-  seoulDateElement.innerHTML = seoulTime.format("MMMM Do, YYYY");
-  seoulTimeElement.innerHTML = seoulTime.format("h:mm:ss [<small>]a[</small>]");
+    seoulDateElement.innerHTML = seoulTime.format("MMMM Do, YYYY");
+    seoulTimeElement.innerHTML = seoulTime.format(
+      "h:mm:ss [<small>]a[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
