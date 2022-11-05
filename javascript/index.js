7181ff94f9ddd51a843f7708124b7875babf735a
Iris Linares <iriscuello@yahoo.com>
2022-11-04 20:07:31 -0400

    added moment time zone

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,15 @@
 let shangElement = document.querySelector("#city-shang");
 let shangDateElement = document.querySelector(".date");
 let shangTimeElement = document.querySelector(".time");
-let shangTime = moment();
+let shangTime = moment().tz("Asia/Shanghai");
 
 shangDateElement.innerHTML = shangTime.format("MMMM Do, YYYY");
 shangTimeElement.innerHTML = shangTime.format("h:m:ss [<small>]a[</small>]");
+
+let seoulElement = document.querySelector("#city-seoul");
+let seoulDateElement = document.querySelector(".date");
+let seoulTimeElement = document.querySelector(".time");
+let seoulTime = moment().tz("Asia/Seoul");
+
+seoulDateElement.innerHTML = seoulTime.format("MMMM Do, YYYY");
+seoulTimeElement.innerHTML = seoulTime.format("h:m:ss [<small>]a[</small>]");
