218e232834942630d5ec00b25674c152333773f1
Iris Linares <iriscuello@yahoo.com>
2022-11-04 20:31:11 -0400

    update html
    added function to update clock every second

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,20 @@
-let shangElement = document.querySelector("#city-shang");
-let shangDateElement = document.querySelector(".date");
-let shangTimeElement = document.querySelector(".time");
-let shangTime = moment().tz("Asia/Shanghai");
+function realTime() {
+  let shangElement = document.querySelector("#city-shang");
+  let shangDateElement = document.querySelector(".date");
+  let shangTimeElement = document.querySelector(".time");
+  let shangTime = moment().tz("Asia/Shanghai");
 
-shangDateElement.innerHTML = shangTime.format("MMMM Do, YYYY");
-shangTimeElement.innerHTML = shangTime.format("h:mm:ss [<small>]a[</small>]");
+  shangDateElement.innerHTML = shangTime.format("MMMM Do, YYYY");
+  shangTimeElement.innerHTML = shangTime.format("h:mm:ss [<small>]a[</small>]");
 
-let seoulElement = document.querySelector("#city-seoul");
-let seoulDateElement = document.querySelector(".date");
-let seoulTimeElement = document.querySelector(".time");
-let seoulTime = moment().tz("Asia/Seoul");
+  let seoulElement = document.querySelector("#city-seoul");
+  let seoulDateElement = document.querySelector(".date");
+  let seoulTimeElement = document.querySelector(".time");
+  let seoulTime = moment().tz("Asia/Seoul");
 
-seoulDateElement.innerHTML = seoulTime.format("MMMM Do, YYYY");
-seoulTimeElement.innerHTML = seoulTime.format("h:mm:ss [<small>]a[</small>]");
+  seoulDateElement.innerHTML = seoulTime.format("MMMM Do, YYYY");
+  seoulTimeElement.innerHTML = seoulTime.format("h:mm:ss [<small>]a[</small>]");
+}
+
+realTime();
+setInterval(realTime, 1000);
